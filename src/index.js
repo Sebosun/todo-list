2c3deb457ff32adb3ef392e7d0c5aa9bfe1b3c2a
Sebosun <sebos@protonmail.com>
2020-09-14 22:25:26 +0200

    fixed localStorage, now works correctly and will overwrite TodoStorage if theres anything in it

--- src/index.js
+++ src/index.js
@@ -3,38 +3,25 @@ import generateTodo from './generateList';
 const titleInput = document.getElementById("title")
 const descriptionInput = document.getElementById('description')
 const endDateInput = document.getElementById('enddate')
-const form = document.getElementById('form');
+const form = document.getElementById('add-item-form');
 
-const contentSelector = document.querySelector('#grid-container');
+//const contentSelector = document.querySelector('#grid-container');
 
 let TodoStorage = {
 }; 
 
-let TodoStorage_deserialized = JSON.parse(localStorage.getItem("TodoStorage"));
-
-console.log(TodoStorage_deserialized);
-
+// basic ToDo Factory, creates one single ToDo object
 const TodoFactory = (title, description, prority, dueDate) => {
     return {title, description, prority, dueDate};
 };
 
-let first = TodoFactory('Pies', 'wyjsc z psem', 'high', '15:40');
-let second = TodoFactory ('Sasiad', 'odwiedzic sasiada', 'low', '19:40');
-let third = TodoFactory('Kupcia', 'zrobic duze pupu', 'Critical', 'now!');
 
-TodoStorage['test'] = [first];
-TodoStorage['test'].push(second, third);
-
-let fourth = TodoFactory('Koteg', 'wyjsc z psem', 'high', '15:40');
-let fifth = TodoFactory ('Srmomsiod', 'odwiedzic sasiada', 'low', '19:40');
-let sixth = TodoFactory('Hedidnto', 'Naprawde dusze', 'Critical', 'now!');
-
-TodoStorage['test2'] = [];
-TodoStorage['test2'].push(fourth, fifth, sixth);
-
-
-generateTodo(TodoStorage_deserialized);
-// generateTodo(TodoStorage_deserialized);
+// Checks if ToDoStorage deserialized is empty. If it's not, 
+// replaces the content of ToDoStorage with user's previous data
+let TodoStorage_deserialized = JSON.parse(localStorage.getItem("TodoStorage"));
+if (TodoStorage_deserialized != null){
+    TodoStorage = JSON.parse(localStorage.getItem("TodoStorage"));
+}
 
 // When the form is submitted, new object is created then added to TodoStorage
 // TodoStorage then is conversted into serialized JSON, which is saved in localStorage
@@ -46,3 +33,4 @@ form.addEventListener('submit', (e) => {
     localStorage.setItem("TodoStorage", TodoStorage_serialized);
 });
 
+generateTodo(TodoStorage);
